60ccd610a599b9070128ad6476107a2d868c4500
Sayali Gayke <sayaligayke810@gmail.com>
2026-06-27 13:24:55 +0530

    add insertion sort

---
+++ Learn Important Sorting Techniques/Sorting-I/InsertionSort.java
@@ -0,0 +1,23 @@
+
+public class InsertionSort {
+  public static void insertionSort(int arr[]) {
+    int n = arr.length;
+    for (int i = 1; i < n; i++) {
+      int key = arr[i];
+      int j = i - 1;
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = key;
+    }
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 22, 1223, 11, 3, 22, 2, 0, 3, 4, 1 };
+    insertionSort(arr);
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
